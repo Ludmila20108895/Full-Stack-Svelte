848f1cca23c6126c9f0a8acb477f4d7b42de6e21
Ludmila20108895 <20108895@mail.wit.ie>
2025-05-29 00:12:29 +0100

    Fix build: use svelte-kit build and allow .svelte-kit output

---
+++ .svelte-kit/generated/client/nodes/3.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../../src/routes/added-places/[id]/+page.svelte";
---
+++ .svelte-kit/types/src/routes/favourites/proxy+page.server.ts
@@ -0,0 +1,30 @@
+// @ts-nocheck
+import type { PageServerLoad } from './$types';
+import { redirect } from '@sveltejs/kit';
+import { connectToDatabase } from '$lib/db';
+import { Poi } from '$lib/server/models/poi';
+
+export const load = async ({ locals }: Parameters<PageServerLoad>[0]) => {
+	if (!locals.user) throw redirect(302, '/login');
+
+	await connectToDatabase();
+
+	const favourites = await Poi.find({
+		createdBy: locals.user.id,
+		favourite: true
+	}).lean();
+
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	const formatted = favourites.map((p: any) => ({
+		_id: p._id?.toString() || '',
+		name: p.name,
+		visitDate: new Date(p.visitDate).toLocaleDateString('en-GB'),
+		latitude: p.latitude,
+		longitude: p.longitude,
+		category: p.category
+	}));
+
+	return {
+		favourites: formatted
+	};
+};
